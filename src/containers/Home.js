18c145bf8139898b6ffbfaaeb92e51b97f04b7eb
Glen Maddern <glenmaddern@gmail.com>
2018-07-24 21:10:01 +1000

    using the symlink

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,7 +3,7 @@ import { withSiteData } from 'react-static'
 //
 import logoImg from '../logo.png'
 import styled from 'styled-components'
-import { typography } from 'linc-front-end/src/styles/traits/index'
+import { typography } from '../lfe/styles/traits/index'
 
 const H1 = styled.h1`
   ${ typography.f1.positive };
